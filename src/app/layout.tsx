f70b985ae64dacd3e60f9674d9240b7a21e19d4e
tvsmith018 <tvsmith0182@gmail.com>
2025-05-19 14:06:20 -0400

    banner added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,6 @@ export default async function RootLayout({
         <Script src="/assets/vendor/sticky-js/sticky.min.js" strategy="afterInteractive"/>
         <Script src="/assets/js/functions.js" strategy="afterInteractive"/>
         <DynmaicBanner/>
-        <Script type='text/javascript' src='//spaniardinformationbookworm.com/f8/95/72/f89572cab58322857c9e20409f4c9433.js' strategy="afterInteractive" />
       </body>
     </html>
   );
