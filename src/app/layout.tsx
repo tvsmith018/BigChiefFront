a18a0170de0086f8a2bf7e677ccf6412313ea56a
tvsmith018 <tvsmith0182@gmail.com>
2025-05-19 12:30:18 -0400

    banner added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import 'swiper/css';
 import "./globals.css"
 
 const DynamicNav = dynamic(async ()=> import('../_views/navigation/NavigationView'))
-// const DynmaicBanner = dynamic(async () => import('@/_views/ads/adsterrabanner'))
+const DynmaicBanner = dynamic(async () => import('@/_views/ads/adsterrabanner'))
 // const inter = Inter({ subsets: ["latin"] });
 
 export const viewport: Viewport = {
@@ -59,7 +59,7 @@ export default async function RootLayout({
         {children}
         <Script src="/assets/vendor/sticky-js/sticky.min.js" strategy="afterInteractive"/>
         <Script src="/assets/js/functions.js" strategy="afterInteractive"/>
-        {/* <DynmaicBanner/> */}
+        <DynmaicBanner/>
         {/* <Script type='text/javascript' src='//spaniardinformationbookworm.com/f8/95/72/f89572cab58322857c9e20409f4c9433.js' strategy="afterInteractive" /> */}
       </body>
     </html>
