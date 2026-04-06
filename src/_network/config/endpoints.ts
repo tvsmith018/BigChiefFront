8cc7a5a8daaf469e547719be27dadbb31194a38e
tvsmith018 <tvsmith0182@gmail.com>
2026-04-05 20:44:21 -0400

    codex update

--- src/_network/config/endpoints.ts
+++ src/_network/config/endpoints.ts
@@ -1,10 +1,15 @@
-const rawApiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
+const DEFAULT_API_BASE_URL = "https://bigchiefnewz-a2e8434d1e6d.herokuapp.com";
 
-if (!rawApiBaseUrl) {
-  throw new Error("Missing NEXT_PUBLIC_API_URL environment variable.");
+function normalizeApiBaseUrl(value: string) {
+  return value.replace(/\/graphql\/?$/, "").replace(/\/+$/, "");
 }
 
-export const API_BASE_URL = rawApiBaseUrl.replace(/\/+$/, "");
+const rawApiBaseUrl =
+  process.env.NEXT_PUBLIC_API_URL ??
+  process.env.NEXT_PUBLIC_ARTICLEURL ??
+  DEFAULT_API_BASE_URL;
+
+export const API_BASE_URL = normalizeApiBaseUrl(rawApiBaseUrl);
 export const GRAPHQL_URL = `${API_BASE_URL}/graphql/`;
 
 export const AUTH_ENDPOINTS = {
